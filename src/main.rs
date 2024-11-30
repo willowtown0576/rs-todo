0eae142d5448834f6964b3049e266f24c0feff24
Keisuke Yanagimachi <willowtown0576@gmail.com>
2024-11-30 22:28:19 +0900

    change port number

--- src/main.rs
+++ src/main.rs
@@ -102,7 +102,7 @@ async fn main() -> std::io::Result<()> {
             .service(todo)
             .app_data(web::Data::new(pool.clone()))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 10000))?
     .run()
     .await
 }
